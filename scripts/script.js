3ac3d9c5f6e2a924c83a9a2d77187be8e47a1afc
RAYAPATI DIVYASREE <divyasailu01@gmail.com>
2023-12-05 20:55:27 +0530

    Designed Sign-in and Sign-up Sections

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+function showSignInForm() {
+  document.getElementById("sign-in-form").style.display = "none";
+  document.getElementById("sign-up-form").style.display = "flex";
+}
+
+function showSignUpForm() {
+  document.getElementById("sign-up-form").style.display = "none";
+  document.getElementById("sign-in-form").style.display = "flex";
+}
