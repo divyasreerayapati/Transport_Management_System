73bbbc30a0b62fc33cb395dee8a20c800c6ec0fc
RAYAPATI DIVYASREE <divyasailu01@gmail.com>
2023-12-06 21:00:57 +0530

    Slot-Booking-Updated

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,15 @@ function showSignUpForm() {
   document.getElementById("sign-up-form").style.display = "none";
   document.getElementById("sign-in-form").style.display = "flex";
 }
+
+function pickUporDrop() {
+  document.getElementById("slots-book").style.display = "flex";
+  var x = document.getElementById("slot-booking").value;
+  if (x === "pickup") {
+    document.getElementById("drop-slot").style.display = "none";
+    document.getElementById("pickup-slot").style.display = "grid";
+  } else {
+    document.getElementById("pickup-slot").style.display = "none";
+    document.getElementById("drop-slot").style.display = "grid";
+  }
+}
